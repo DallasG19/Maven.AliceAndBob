42ef318d7c2ba6daf02a80b55c574b0d16fc4924
DallasG19 <gdallas36@gmail.com>
2020-09-15 10:01:05 -0400

    first

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
 
-    public static void main(String[] args ){
-
+    }        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        name.toLowerCase();
+        if(name == "alice"){
+            System.out.println("Welcome Alice");
+            break;
+        }else if(name == "bob"){
+            System.out.println("Welcome Bob");
+            break;
+        }
     }
 }
